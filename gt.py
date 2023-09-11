980ede3602059a2878221f70e74866d4d458ffe1
demqv123 <144516410+demqv123@users.noreply.github.com>
2023-09-11 17:21:17 +0900

    Update gt.py

--- gt.py
+++ gt.py
@@ -1,2 +1,1 @@
-Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+print("제 이름은 심형민이고 학번은 202310618 입니다")
